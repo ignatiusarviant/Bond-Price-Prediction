07ca82095c466db32c99d08868c9c530b99b8074
ignatiusarviant <ignatiusarviantt@gmail.com>
2025-07-29 18:14:43 +0700

    Update Test_data_processing.py

--- Test_data_processing.py
+++ Test_data_processing.py
@@ -42,13 +42,12 @@ df["Momentum 3d"] = df["Price"].diff(3)
 df["MA 5d"] = df["Price"].rolling(window=5).mean()
 df["MA 10d"] = df["Price"].rolling(window=10).mean()
 df["Volatility 5d"] = df["Price Return 1d"].rolling(window=5).std()
-df["Interest Rate"] = df.apply(interest_rate_addition, axis=1)
 df["Change %"] = df["Change %"].str.replace("%", "").astype(float)
 
 df = df[[
     "Date", "Price", "Price Lag 1", "Price Lag 2", "Price Return 1d",
     "Momentum 3d", "MA 5d", "MA 10d", "Volatility 5d",
-    "Change %", "Interest Rate"
+    "Change %"
 ]]
 
 df["Date"] = pd.to_datetime(df["Date"])
